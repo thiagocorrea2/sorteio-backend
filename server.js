63ebba1b77930f757e4ba959d1c22a486cd24185
thiagocorrea2 <conectiptv079@gmail.com>
2025-05-24 09:29:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { mongoURI } = require('./config');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
